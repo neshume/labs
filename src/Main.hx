d6c5e861486706ede74dc7eb96791bb85c7eb280
Sébastien Bénard <fork@deepnight.net>
2019-10-18 10:45:07 +0200

    Added music test files

--- src/Main.hx
+++ src/Main.hx
@@ -1,9 +1,29 @@
 class Main extends dn.Process {
 	public static var ME : Main;
 
+	var music : hxd.snd.Channel;
+	var indicator : h2d.Bitmap;
+
 	public function new(scene:h2d.Scene) {
 		super();
 		ME = this;
 		createRoot(scene);
+
+		indicator = new h2d.Bitmap(h2d.Tile.fromColor(0x00ff00), root);
+		indicator.setPosition(50,50);
+		indicator.scale(100);
+
+		music = hxd.Res.music.play();
+		music.volume = 0.4;
 	}
+
+	override function update() {
+		super.update();
+
+		if( hxd.Key.isPressed(hxd.Key.SPACE) ) {
+			music.pause = !music.pause;
+			indicator.tile = h2d.Tile.fromColor( music.pause ? 0xff0000 : 0x00ff00 );
+		}
+	}
+
 }
