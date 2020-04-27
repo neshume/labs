1e93037be9d0ab9066ab453b71e19ebae0332940
Sébastien Bénard <github@deepnight.net>
2020-04-27 08:30:54 +0200

    Fixes

--- filePath/src/Main.hx
+++ filePath/src/Main.hx
@@ -10,31 +10,26 @@ class Main extends hxd.App {
 	override function init() {
 		ME = this;
 
-		test("");
-		test("/project.seb/rpgmap");
-		test("/project.seb/rpgmap/");
-		test("//project.seb//rpgmap/");
-		test("/project.seb/rpgmap/LICENSE.md");
-		test("/project.seb/rpgmap/test.png");
-		test("/project.seb/rpgmap/another.test.png");
+		test("../test.png");
+		test("/project.seb/rpgmap/myApp.isCool.exe");
 		test("c:\\windows\\system\\test.dll");
 
 		hxd.System.exit();
 	}
 
 	function test(path:String) {
+		trace("______________________________");
 		trace("TESTING "+path);
 		trace( FP.fromFile(path).debug() );
-		trace( "dirSlash (noFile=false)="+FP.getDirWithSlash(path,false) );
-		trace( "dirSlash (noFile=true)="+FP.getDirWithSlash(path,true) );
-		trace( "file="+FP.getFile(path) );
-		trace( "ext="+FP.getExt(path) );
-		trace( "fileExt="+FP.getFileWithExt(path) );
+		trace( "dirSlash (noFile=false)="+FP.extractDirWithSlash(path,false) );
+		trace( "dirSlash (noFile=true)="+FP.extractDirNoSlash(path,true) );
+		trace( "file="+FP.extractFileName(path) );
+		trace( "ext="+FP.extractExtension(path) );
+		trace( "fileExt="+FP.extractFileWithExt(path) );
 
 		var p = FP.fromFile(path);
 		p.fileWithExt = "override.json";
 		trace("override="+p.full);
-		trace("");
 	}
 
 	override function update(deltaTime:Float) {
